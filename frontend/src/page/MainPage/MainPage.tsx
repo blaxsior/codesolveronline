138acf2b93409577d1dbb5e562633a507d94786f
blaxsior <heejun2798@gmail.com>
2022-12-01 13:03:20 +0900

    메인페이지 간단하게 꾸밈

--- frontend/src/page/MainPage/MainPage.tsx
+++ frontend/src/page/MainPage/MainPage.tsx
@@ -1,6 +1,17 @@
+import styles from './MainPage.module.css';
 
-const MainPage : React.FC = (props) => {
-    return <div>hello!</div>;
+const MainPage: React.FC = (props) => {
+    return (
+        <div className="page-layout">
+            <img className={styles['logo']} 
+            src='/cso_logo.png'
+            alt='code solver online page logo'/>
+            <div>
+                <p>
+                    온라인 기반 코딩 문제 풀이 시스템
+                </p>
+            </div>
+        </div>);
 }
 
 export default MainPage;
